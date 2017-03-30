ecfead6093fff5c0c4461e6ca0e2656dffcaeb47
mdilorenzo <matthewdilorenzo@gmail.com>
2017-03-30 12:06:32 -0400

    Add purpose to target weights

---
+++ R/pt-wts-data.R
@@ -0,0 +1,14 @@
+#' Matrix of weights for purpose code to SDG target links
+#'
+#'
+#' @docType data
+#'
+#' @usage data(pt_wts)
+#'
+#' @format 
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(pt_wts)
+"pt_wts"
