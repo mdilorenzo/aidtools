abaf997261f402f3415f28ed6b1ea993570f775a
mdilorenzo <matthewdilorenzo@gmail.com>
2017-03-30 10:10:11 -0400

    Add activity to target weights

---
+++ R/at-wts-data.R
@@ -0,0 +1,14 @@
+#' Matrix of weights for activity code to SDG target links
+#'
+#'
+#' @docType data
+#'
+#' @usage data(at_wts)
+#'
+#' @format 
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(at_wts)
+"at_wts"
--- R/sdg-coding-functions.R
+++ R/sdg-coding-functions.R
@@ -14,7 +14,6 @@ sdg_coder <- function(dat, single_activity = FALSE, coalesced_purpose = FALSE){
   ## Make sure dollar amounts are numeric
   dat$commitment_amount_usd_constant <- as.numeric(as.character(dat$commitment_amount_usd_constant))
   
-  
   ## Logical check of whether to use coalesced purpose codes
   if(coalesced_purpose == TRUE){
     
@@ -99,5 +98,108 @@ sdg_coder <- function(dat, single_activity = FALSE, coalesced_purpose = FALSE){
 }
 
 
+
+
+
+
+
+
+#' A Function for Getting Target-level SDG Estimates for Projects with AidData Activity Codes
+#'
+#' Get project-level estimates of contributions to SDGs. This function requires three input variables with specific names: aiddata_id, aiddata_activity_codes, and commitment_amount_usd_constant. If coalesced_purpose == TRUE, then there should be a column called coalesced_purpose_code.
+#' @param dat Data frame containing projects and financial amounts. Name of activity codes variable assumed to be "aiddata_activity_codes" and financial amount variable "commitment_amount_usd_constant".
+#' @param single_activity Logical value indicating whether all the projects in the data frame have only a single activity. Can use activity_counter() to pre-filter data. By default the function can deal with cases that have single or multiple activities, but applying the function to cases with single activities takes more time than is necessary. If TRUE, run on subset of data without multiple activity codes and bind with other data later. 
+#' @param coalesced_purpose Logical value indicating whether function should use alternative method of coalesced purpose code to SDG weights (p_wts). If TRUE, run on subset of data without activity codes and bind with other data later. 
+#' @keywords 
+#' @export
+#' @examples
+#' sdg_coder(dat = aiddata_core_research_release, single_activity = FALSE)
+sdg_target_coder <- function(dat, single_activity = FALSE, coalesced_purpose = FALSE){
+  
+  ## Make sure dollar amounts are numeric
+  dat$commitment_amount_usd_constant <- as.numeric(as.character(dat$commitment_amount_usd_constant))
+  
+  ## Logical check of whether to use coalesced purpose codes
+  if(coalesced_purpose == TRUE){
+    
+    dat$coalesced_purpose_code <- as.character(dat$coalesced_purpose_code)
+    p_wts$coalesced_purpose_code <- as.character(p_wts$coalesced_purpose_code)
+    
+    ## Merge, multiply by dollar amounts, select
+    merged <- left_join(dat, p_wts, by = "coalesced_purpose_code") %>%
+      mutate_each(funs(.*commitment_amount_usd_constant), starts_with("goal_")) %>%
+      select(aiddata_id, goal_1:goal_17)
+    
+    merged[is.na(merged)] <- 0
+    
+    return(merged)
+    
+  }
+  
+  ## Check single_activity status. If TRUE, simple merge and multiplier.
+  if(single_activity == TRUE){
+    
+    ## Rename activity code variable to match
+    wts <- wts %>%
+      rename(aiddata_activity_codes = activity_code)
+    
+    ## Merge, multiply by dollar amounts, select
+    merged <- left_join(dat, wts, by = "aiddata_activity_codes") %>%
+      mutate_each(funs(.*commitment_amount_usd_constant), starts_with("goal_")) %>%
+      select(aiddata_id, goal_1:goal_17)
+    
+    merged[is.na(merged)] <- 0
+    
+    return(merged)
+    
+  }
+  
+  ## If single_activity == FALSE, split projects up by activities
+  if(single_activity == FALSE){
+    
+    ## Loop through rows
+    result <- foreach(i = 1:nrow(dat), .combine = "rbind") %do% {
+      
+      ## Set amount to zero to start
+      amt <- 0
+      
+      ## Get vector of activity codes
+      act <- unlist(c(strsplit(dat$aiddata_activity_codes[i],
+                               "|",
+                               fixed = TRUE))) %>% unique()
+      
+      ## Split project amounts into number of unique activities
+      if(length(act) > 0){
+        
+        amt <- dat$commitment_amount_usd_constant[i] / length(act)
+        
+      }
+      
+      ## Create a matrix of the activity-to-goal weights for relevant activities
+      links <- wts %>%
+        filter(activity_code %in% act) %>%
+        select(-activity_code)
+      
+      ## If no matches, create vector of zeroes of length 17
+      if(nrow(links) < 1){
+        
+        links <- matrix(rep(0, 17), nrow = 1, ncol = 17)
+        colnames(links) <- paste("goal", 1:17, sep = "_")
+        
+      }
+      
+      ## Generate vector of SDG estimates
+      sdg_estimates <- colSums(links * amt)
+      
+      ## Concatenate with project_id
+      c(aiddata_id = dat$aiddata_id[i], sdg_estimates)
+      
+    }
+    
+    return(result)
+    
+  }
+  
+}
 
 
